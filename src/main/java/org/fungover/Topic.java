e0b059eb04c5eb41073a8c8aaf67dfc0f3e55b5b
cybertobbe <143131053+cybertobbe@users.noreply.github.com>
2024-02-05 18:53:54 +0100

    Add test for forward slash, add equals and hashcode methods.
    Co-authored-by: Ingrid Hammargren <ingrid.hammargren@iths.se>
    Co-authored-by: Katherine Altmann <katherine.altmann@iths.se>
    Co-authored-by: Tobias Sjöström <tobias.sjostrom@iths.se>

--- src/main/java/org/fungover/Topic.java
+++ src/main/java/org/fungover/Topic.java
@@ -1,21 +1,29 @@
 package org.fungover;
 
-public class Topic {
+import java.util.Objects;
 
+public class Topic {
     String name;
 
-    public Topic(String name) {
+    private Topic (String name) {
         this.name = name;
-    }
+   }
 
-    public static Topic createTopic(String topicName) {
-        if(topicName == null || topicName.isEmpty())
-            throw new IllegalArgumentException("Invalid topic, empty String");
+    public static Topic create(String topicName) {
+        if(topicName == null || topicName.isEmpty() || topicName.startsWith("/"))
+            throw new IllegalArgumentException("Invalid topic");
         return new Topic(topicName);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Topic topic)) return false;
+        return Objects.equals(name, topic.name);
+    }
 
-    public boolean isExactMatch(Topic other) {
-        return this.name.equals(other.name);
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
--- src/test/java/com/fungover/TopicTest.java
+++ src/test/java/com/fungover/TopicTest.java
@@ -13,32 +13,38 @@ public class TopicTest{
     @Test
     @DisplayName("Exact match when given two topics")
     void exactMatchWhenGivenTwoTopics() {
-        Topic topic1 =  Topic.createTopic ("myhome/first_floor/kitchen/temperature");
-        Topic topic2 = Topic.createTopic ("myhome/first_floor/kitchen/temperature");
+        Topic topic1 =  Topic.create ("myhome/first_floor/kitchen/temperature");
+        Topic topic2 = Topic.create("myhome/first_floor/kitchen/temperature");
 
-        assertThat(topic1.isExactMatch(topic2)).isTrue();
+        assertThat(topic1.equals(topic2)).isTrue();
 
     }
 
     @Test
     @DisplayName("Not exact match when given two topics")
     void notExactMatchWhenGivenTwoTopics() {
-        Topic topic1 = Topic.createTopic("myhome/first_floor/kitchen/temperature");
-        Topic topic2 = Topic.createTopic("myhome/first_floor/kitchen/");
+        Topic topic1 = Topic.create("myhome/first_floor/kitchen/temperature");
+        Topic topic2 = Topic.create("myhome/first_floor/kitchen/");
 
-        assertThat(topic1.isExactMatch(topic2)).isFalse();
+        assertThat(topic1.equals(topic2)).isFalse();
 
     }
 
     @Test
     @DisplayName("Creating topic with empty string should throw IllegalArgumentException")
     void creatingTopicWithEmptyStringShouldThrowIllegalArgumentException() {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,()-> Topic.createTopic(""));
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,()-> Topic.create(""));
 
-        assertThat(exception).hasMessage("Invalid topic, empty String");
+        assertThat(exception).hasMessage("Invalid topic");
 
     }
 
+    @Test
+    @DisplayName("Topic can not start with forward slash")
+    void topicCanNotStartWithForwardSlash() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> Topic.create("/invalid/topic"));
 
+        assertThat(exception).hasMessage("Invalid topic");
+    }
 
 }
