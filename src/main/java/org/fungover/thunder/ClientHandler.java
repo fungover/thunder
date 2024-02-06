f6f5415799b6839d386c9d166c54bbd1ae8989fe
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-02-06 20:19:47 +0100

    Refactor clienthandler

--- src/main/java/org/fungover/thunder/ClientHandler.java
+++ src/main/java/org/fungover/thunder/ClientHandler.java
@@ -1,10 +1,6 @@
 package org.fungover.thunder;
 
-import java.io.IOException;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -34,5 +30,4 @@ public class ClientHandler {
     public List<Socket> getClients() {
         return clients;
     }
-
 }
