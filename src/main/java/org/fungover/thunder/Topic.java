dffb6ff3144e24200b8dc225f95c473f6ff4d721
altmannk <katherine.altmann@iths.se>
2024-02-08 22:05:08 +0100

    Refactor: extract code from create to new method checkTopicNamingValidation
    Co-authored-by: Ingrid Hammargren <ingrid.hammargren@iths.se>
    Co-authored-by: Tobias Sjöström <tobias.sjostrom@iths.se>
    Co-authored-by: Katherine Altmann <katherine.altmann@iths.se>

--- src/main/java/org/fungover/thunder/Topic.java
+++ src/main/java/org/fungover/thunder/Topic.java
@@ -4,16 +4,22 @@ public record Topic(String name) {
 
 
     public static Topic create(String topicName) {
+        checkTopicNamingValidation(topicName);
+        return new Topic(topicName);
+    }
+
+    private static void checkTopicNamingValidation(String topicName) {
         if (topicName == null || topicName.isEmpty())
             throw new IllegalArgumentException("Invalid topic: Should contain at least one character");
+
         if (topicName.startsWith("/"))
             throw new IllegalArgumentException("Invalid topic: Can not begin with '/'");
+
         if (topicName.contains("#")) {
             int count = countAmountOfMultiLevelWildcard(topicName);
             if (count == 1 && !topicName.endsWith("/#") || count > 1)
                 throw new IllegalArgumentException("Invalid topic: Multi-level wildcard (#) must be placed as the last character in the topic, preceded by a forward slash");
         }
-        return new Topic(topicName);
     }
 
     // Checks how many '#' there is in topic
@@ -27,6 +33,7 @@ public record Topic(String name) {
         return count;
     }
 
+    // check two topic names to match wildcard
     public boolean matchesWildcard(String otherTopic) {
         String[] thisLevels = this.name.split("/");
         String[] otherLevels = otherTopic.split("/");
