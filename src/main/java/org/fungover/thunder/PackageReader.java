410731f98051b0fe99f236698d165d2b08c433b5
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-02-08 20:26:10 +0100

    Add subscription field and add readFromClient method to process packages

--- src/main/java/org/fungover/thunder/PackageReader.java
+++ src/main/java/org/fungover/thunder/PackageReader.java
@@ -10,6 +10,7 @@ import java.util.Map;
 
 public class PackageReader {
     private final Map<InetAddress, Boolean> connectPackageSent = new HashMap<>();
+    private final Subscription subscription = new Subscription();
     private static final int CONNECTION_TIMEOUT = 30000;
 
     public boolean isValidConnection(Socket socket) throws IOException {
@@ -17,13 +18,12 @@ public class PackageReader {
         byte[] buffer = new byte[1024];
         InputStream inputStream = socket.getInputStream();
         OutputStream outputStream = socket.getOutputStream();
+        socket.setSoTimeout(CONNECTION_TIMEOUT);
         int bytesRead = inputStream.read(buffer);
 
         if (isDoubleConnectMessage(client, buffer)) {
             return false;
         }
-
-        socket.setSoTimeout(CONNECTION_TIMEOUT);
 
         if (isConnectPackage(bytesRead, buffer)) {
             System.out.println("Received MQTT CONNECT message from client");
@@ -32,11 +32,36 @@ public class PackageReader {
             System.out.println("Sent MQTT CONNACK message to client");
             return true;
         }
+
         return false;
     }
 
     private boolean isDoubleConnectMessage(InetAddress client, byte[] buffer) {
         return connectPackageSent.containsKey(client) && Boolean.TRUE.equals(connectPackageSent.get(client)) && buffer[0] == 0x10;
+    }
+
+    private static boolean isConnectPackage(int bytesRead, byte[] buffer) {
+        return bytesRead > 0 && buffer[0] == (byte) 0x10;
+    }
+
+    public boolean readFromClient(Socket clientSocket) throws IOException {
+        byte[] buffer = new byte[1024];
+        InputStream inputStream = clientSocket.getInputStream();
+        OutputStream outputStream = clientSocket.getOutputStream();
+        clientSocket.setSoTimeout(CONNECTION_TIMEOUT);
+        int bytesRead = inputStream.read(buffer);
+
+        if (buffer[0] == 0x10) {
+            System.out.println("Sending connection package after connection is established is forbidden.");
+            return false;
+        }
+
+        if (buffer[0] == (byte) 0x82) {
+            subscription.read(bytesRead, buffer, clientSocket);
+            sendSubackToClient(outputStream);
+        }
+
+        return false;
     }
 
     private static void sendConnackToClient(OutputStream outputStream) throws IOException {
@@ -45,7 +70,10 @@ public class PackageReader {
         outputStream.flush();
     }
 
-    private static boolean isConnectPackage(int bytesRead, byte[] buffer) {
-        return bytesRead > 0 && buffer[0] == (byte) 0x10;
+    private void sendSubackToClient(OutputStream outputStream) throws IOException {
+        byte[] connackMessage = new byte[]{(byte) 0x20, (byte) 0x02, (byte) 0x00, (byte) 0x00};
+        outputStream.write(connackMessage);
+        outputStream.flush();
     }
+
 }
