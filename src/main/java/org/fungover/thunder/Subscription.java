8af68e48e7deabf9ec19473d94ba46f70f42c996
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-02-08 20:27:09 +0100

    Update add to subscription to return boolean. Refactor variablename

--- src/main/java/org/fungover/thunder/Subscription.java
+++ src/main/java/org/fungover/thunder/Subscription.java
@@ -7,27 +7,30 @@ import java.util.*;
 public class Subscription {
     private Map<String, List<Socket>> subscriptions;
 
-    Subscription(){
+    Subscription() {
         this.subscriptions = new HashMap<>();
     }
 
-    public void read(int length,byte[] buffer, Socket socket) {
-        byte[] copy = Arrays.copyOfRange(buffer,1,length);
-        String topicFilter = new String(copy, StandardCharsets.UTF_8);
-        addToSubscription(topicFilter, socket);
+    public void read(int length, byte[] buffer, Socket socket) {
+        byte[] copy = Arrays.copyOfRange(buffer, 1, length);
+        String byteToTopicString = new String(copy, StandardCharsets.UTF_8);
+        addToSubscription(byteToTopicString, socket);
         getSubscriptions();
     }
 
-    public void addToSubscription(String topicFilter, Socket socket) {
+    public boolean addToSubscription(String topicFilter, Socket socket) {
         List<Socket> sockets = new ArrayList<>();
         if (subscriptions.containsKey(topicFilter)) {
             var list = subscriptions.get(topicFilter);
             list.add(socket);
-        } else {
+            return true;
+        } else if(!subscriptions.containsKey(topicFilter)){
             sockets.add(socket);
             Topic topic = new Topic(topicFilter);
             subscriptions.put(topic.getName(), sockets);
+            return true;
         }
+        return false;
     }
 
     public Map<String, List<Socket>> getSubscriptions() {
@@ -36,10 +39,11 @@ public class Subscription {
             List<Socket> sockets = entry.getValue();
 
             System.out.println("Topic: " + topic);
-            System.out.println("Sockets:");
+            System.out.println("Sockets subscribing on topic:");
             for (Socket socket : sockets) {
                 System.out.println(socket);
             }
+            System.out.println("---------------------");
         }
         return subscriptions;
     }
