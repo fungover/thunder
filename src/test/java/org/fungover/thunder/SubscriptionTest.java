4c9eda0b3eaf3460ec5d49ceec4f44ee4f24fd43
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-02-13 22:31:29 +0100

    Refactor tests
    Co-authored-by: Marcus Olsson <marcus.olsson98@gmail.com>
    Co-authored-by: Fredrik Lundsten <f.lundsten@hotmail.se>

--- src/test/java/org/fungover/thunder/SubscriptionTest.java
+++ src/test/java/org/fungover/thunder/SubscriptionTest.java
@@ -4,62 +4,39 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.*;
 import java.net.Socket;
-import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 class SubscriptionTest {
 
-    private PackageReader packageReader;
     private Subscription subscription;
+    private Socket socketMock;
 
     @BeforeEach
     void setUp() {
-        packageReader = new PackageReader();
         subscription = new Subscription();
+        socketMock = mock(Socket.class);
     }
 
-
     @Test
-    @DisplayName("Two different topic subscriptions should return map size 2")
-    void twoDifferentTopicSubscriptionsShouldReturnMapSize2(){
-        String topicHelloSlashWorldInBytes = new String(new byte[]{0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x2F, 0x77, 0x6F, 0x72, 0x6C, 0x64},StandardCharsets.UTF_8);
-        String topicWorldInBytes = new String(new byte[]{0x57, 0x6F, 0x72, 0x6C, 0x64},StandardCharsets.UTF_8);
+    @DisplayName("Return subscription size 1 when read method is processed")
+    void returnSubscriptionSize1WhenReadMethodIsProcessed() {
+        subscription.read(11,
+            new byte[]{(byte) 0x82, 0x6C, 0x6C, 0x6F, 0x2F, 0x77, 0x6F, 0x72, 0x6C, 0x64,0x01},
+            socketMock);
 
-        Socket socketMock = mock(Socket.class);
-        subscription.addToSubscription(topicHelloSlashWorldInBytes,socketMock);
-
-        Socket socketMockTwo = mock(Socket.class);
-        subscription.addToSubscription(topicWorldInBytes, socketMockTwo);
-
-        Socket socketMockThree = mock(Socket.class);
-        subscription.addToSubscription(topicWorldInBytes, socketMockThree);
-
-        assertThat(subscription.getSubscriptions()).hasSize(2);
+        assertThat(subscription.getSubscriptions()).hasSize(1);
     }
 
-
     @Test
-    @DisplayName("Return 2 when 2 different topics are added")
-    void return2When2DifferentTopicsAreAdded(){
-        Socket socket = mock(Socket.class);
-        String str = new String(new byte[]{ 0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x2F, 0x77, 0x6F, 0x72, 0x6C, 0x64}, StandardCharsets.UTF_8);
-
-        Socket socket2 = mock(Socket.class);
-        String str2 = new String(new byte[]{ 0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x2F, 0x77, 0x6F, 0x72, 0x6C, 0x64}, StandardCharsets.UTF_8);
-
-        Socket socket3 = mock(Socket.class);
-        String str3 = new String(new byte[]{ 0x42, 0x65, 0x6C, 0x6C, 0x6F, 0x2F, 0x77, 0x6F, 0x72, 0x6C, 0x64}, StandardCharsets.UTF_8);
-
-        subscription.addToSubscription(str,socket);
-        subscription.addToSubscription(str2,socket2);
-        subscription.addToSubscription(str3,socket3);
-
-        assertThat(subscription.getSubscriptions()).hasSize(2);
+    @DisplayName("Throw exception when topic start with /")
+    void throwExceptionWhenTopicStartWith() {
+        assertThrows(IllegalArgumentException.class, () ->
+            subscription.read(6,
+                new byte[]{(byte) 0x82, 0x2F, 0x77, 0x6F, 0x72, 0x6C, 0x64},
+                socketMock));
     }
 }
