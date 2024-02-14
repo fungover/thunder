22075e8ec4b735ff9cd4c7f4fdb7ccbdc08cc6a1
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-02-14 09:58:15 +0100

    Edit loop to check matching topics
    Co-authored-by: Marcus Olsson <marcus.olsson98@gmail.com>
    Co-authored-by: Fredrik Lundsten <f.lundsten@hotmail.se>

--- src/main/java/org/fungover/thunder/Subscription.java
+++ src/main/java/org/fungover/thunder/Subscription.java
@@ -13,23 +13,30 @@ public class Subscription {
 
     public void read(int length, byte[] buffer, Socket socket) {
 
-        byte[] topicFilter  = Arrays.copyOfRange(buffer, 6, length-1);
+        byte[] topicFilter = Arrays.copyOfRange(buffer, 6, length - 1);
 
-            List<Topic> matchingTopics = new ArrayList<>();
-            Topic topic = Topic.create(new String(topicFilter, StandardCharsets.UTF_8), buffer[length-1]);
+        List<Topic> matchingTopics = new ArrayList<>();
+        Topic topic = Topic.create(new String(topicFilter, StandardCharsets.UTF_8), buffer[length - 1]);
 
-            subscriptions.forEach((topicName, socketList) -> {
-                if (topic.matchesWildcard(topic.name())) {
-                    socketList.add(socket);
-                    matchingTopics.add(topicName);
-                }
-            });
-
-            if (topic.isValidForSubscription() && matchingTopics.isEmpty()) {
-                List<Socket> sockets = new ArrayList<>();
-                sockets.add(socket);
-                subscriptions.put(topic, sockets);
+        Iterator<Map.Entry<Topic, List<Socket>>> iterator = subscriptions.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<Topic, List<Socket>> entry = iterator.next();
+            Topic topicName = entry.getKey();
+            if (topic.matchesWildcard(topicName.name())) {
+                matchingTopics.add(topicName);
             }
+        }
+
+        if (topic.isValidForSubscription() && matchingTopics.isEmpty()) {
+            List<Socket> sockets = new ArrayList<>();
+            sockets.add(socket);
+            subscriptions.put(topic, sockets);
+        } else {
+            for (int i = 0; i < matchingTopics.size(); i++) {
+                subscriptions.get(matchingTopics.get(i)).add(socket);
+            }
+        }
+        matchingTopics.clear();
     }
 
     public Map<Topic, List<Socket>> getSubscriptions() {
