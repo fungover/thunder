14b3fbc0d8d68b56cba0a4d00ffbb4affa7b8c02
Fredrik Lundsten <f.lundsten@hotmail.se>
2024-02-15 09:32:14 +0100

    Update map to ConcurrentHashMap with CopyOnWriteArrayList values. refactored read()
    Co-authored-by: Marcus Olsson <marcus.olsson98@gmail.com>
    Co-authored-by: Fredrik Lundsten <f.lundsten@hotmail.se>

--- src/main/java/org/fungover/thunder/Subscription.java
+++ src/main/java/org/fungover/thunder/Subscription.java
@@ -3,38 +3,45 @@ package org.fungover.thunder;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Subscription {
-    private final Map<Topic, List<Socket>> subscriptions;
+    private final ConcurrentHashMap<Topic, List<Socket>> subscriptions;
 
     Subscription() {
-        this.subscriptions = new HashMap<>();
+        this.subscriptions = new ConcurrentHashMap<>();
     }
 
     public void read(int length, byte[] buffer, Socket socket) {
-
         byte[] topicFilter = Arrays.copyOfRange(buffer, 6, length - 1);
 
-        List<Topic> matchingTopics = new ArrayList<>();
+
         Topic topic = Topic.create(new String(topicFilter, StandardCharsets.UTF_8), buffer[length - 1]);
 
+        List<Topic> matchingTopics = listOfMatchedTopics(topic);
+
+        if (topic.isValidForSubscription() && matchingTopics.isEmpty()) {
+            List<Socket> sockets = new CopyOnWriteArrayList<>();
+            sockets.add(socket);
+            subscriptions.putIfAbsent(topic, sockets);
+        } else {
+            for (Topic matchingTopic : matchingTopics) {
+                subscriptions.get(matchingTopic).add(socket);
+            }
+        }
+
+    }
+
+    private List<Topic> listOfMatchedTopics(Topic topic) {
+        List<Topic> matchingTopics = new ArrayList<>();
         for (Map.Entry<Topic, List<Socket>> entry : subscriptions.entrySet()) {
             Topic topicName = entry.getKey();
             if (topic.matchesWildcard(topicName.name())) {
                 matchingTopics.add(topicName);
             }
         }
-
-        if (topic.isValidForSubscription() && matchingTopics.isEmpty()) {
-            List<Socket> sockets = new ArrayList<>();
-            sockets.add(socket);
-            subscriptions.put(topic, sockets);
-        } else {
-            for (Topic matchingTopic : matchingTopics) {
-                subscriptions.get(matchingTopic).add(socket);
-            }
-        }
-        matchingTopics.clear();
+        return List.copyOf(matchingTopics);
     }
 
     public Map<Topic, List<Socket>> getSubscriptions() {
